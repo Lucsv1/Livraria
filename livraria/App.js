12c7706a11f19c4d67d3739b375a04c3c19fa30d
Lucas <lcasgalindo@gmail.com>
2023-05-05 20:52:05 -0300

    metada do projeto

--- livraria/App.js
+++ livraria/App.js
@@ -1,11 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  ImageBackground,
+  SectionList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import img from "./assets/floresta.png";
+import { useState } from "react";
 
 export default function App() {
+
+  const [tituloAutor, setTituloAutor] = useState('')
+  const [nomeAutor, setNomeAutor] = useState('')
+  const [editora, setEditora] = useState('')
+  const [preco,setPreco ] = useState('')
+  const [tipo, setTipo] = useState('')
+  const [lista, setLista] = useState('')
+
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flex: 3, opacity: 1 }}>
+        <ImageBackground
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          source={img}
+          resizeMode="cover"
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 20,
+              color: "white",
+              backgroundColor: "#174817",
+              padding: 10,
+              borderRadius: 10,
+            }}
+          >
+            Livraria Amazonas
+          </Text>
+        </ImageBackground>
+      </View>
+      <View style={{ flex: 7, backgroundColor: "#174817" }}>
+        <View style={{ flex: 1, padding: 10 }}>
+          <Text>dados do livro</Text>
+          <TextInput value={tituloAutor}
+          onChangeText={setTituloAutor}
+            style={{ backgroundColor: "white", textAlign: "left", padding: 2 }}
+            placeholder="titulo do livor"
+          />
+          <TextInput value={nomeAutor}
+          onChangeText={setNomeAutor}
+            style={{ backgroundColor: "white", textAlign: "left", padding: 2 }}
+            placeholder="nome do autor"
+          />
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <TextInput value={editora}
+            onChangeText={setEditora}
+              style={{
+                backgroundColor: "white",
+                textAlign: "left",
+                padding: 2,
+                width: "50%",
+              }}
+              placeholder="editora"
+            />
+            <TextInput value={preco}
+            onChangeText={setPreco}
+              style={{
+                backgroundColor: "white",
+                textAlign: "left",
+                padding: 2,
+                width: "49%",
+              }}
+              placeholder="preço"
+            />
+          </View>
+          <View style={{flexDirection: 'row', justifyContent:"space-between"}}>
+            <TextInput value={tipo}
+            onChangeText={setTipo}
+              style={{
+                backgroundColor: "white",
+                textAlign: "left",
+                padding: 2,
+                width: '50%'
+              }}
+              placeholder="tipo"
+            />
+            <View style={{ backgroundColor: "brown", width: '49%' }}>
+              <Text onPress={()=>{
+                const obj = {tituloAutor, nomeAutor, editora, preco, tipo}
+                setLista([...lista, obj])
+
+              }}  style={{textAlign: 'center'}} >Salvar</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ flex: 2, backgroundColor: 'white' }}>
+          <SectionList 
+          sections={[
+          {title: 'Terror', data: ()=>{if(tipo == 'terror'){return lista}},
+           title: 'aventura', data: ()=>{if(tipo == 'aventura'){return lista}}}
+          ]}
+          renderSectionFooter={({section})=>(
+            <View>
+              <Text>{section.title}</Text>
+              <Text>{section.title}</Text>
+            </View>
+            
+          )}
+          renderItem={({item})=>(
+            <Text>{item.tipo}</Text>
+          )} 
+          keyExtractor={item => item.key}/>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +126,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
