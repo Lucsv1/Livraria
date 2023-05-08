f3de2d734484114f13d02db28e79870724b4e408
Lucas <lcasgalindo@gmail.com>
2023-05-08 09:24:04 -0300

    projeto finalizado

--- livraria/App.js
+++ livraria/App.js
@@ -6,21 +6,23 @@ import {
   Text,
   TextInput,
   View,
+  Image,
+  Modal,
+  TouchableHighlightComponent,
+  TouchableOpacity,
 } from "react-native";
 import img from "./assets/floresta.png";
 import { useState } from "react";
 
 export default function App() {
-
-  const [tituloAutor, setTituloAutor] = useState('')
-  const [nomeAutor, setNomeAutor] = useState('')
-  const [editora, setEditora] = useState('')
-  const [preco,setPreco ] = useState('')
-  const [tipo, setTipo] = useState('')
-  const [tipoTerro, setTipoTerror] = useState([])
-  const [tipoAventira, setTipoAventura] = useState([])
-
-  
+  const [tituloAutor, setTituloAutor] = useState("");
+  const [nomeAutor, setNomeAutor] = useState("");
+  const [editora, setEditora] = useState("");
+  const [preco, setPreco] = useState("");
+  const [tipo, setTipo] = useState("");
+  const [tipoTerro, setTipoTerror] = useState([]);
+  const [tipoAventira, setTipoAventura] = useState([]);
+  const [modal, setModal] = useState(false);
 
   return (
     <View style={styles.container}>
@@ -47,21 +49,24 @@ export default function App() {
       <View style={{ flex: 7, backgroundColor: "#174817" }}>
         <View style={{ flex: 1, padding: 10 }}>
           <Text>dados do livro</Text>
-          <TextInput value={tituloAutor}
-          onChangeText={setTituloAutor}
+          <TextInput
+            value={tituloAutor}
+            onChangeText={setTituloAutor}
             style={{ backgroundColor: "white", textAlign: "left", padding: 2 }}
             placeholder="titulo do livor"
           />
-          <TextInput value={nomeAutor}
-          onChangeText={setNomeAutor}
+          <TextInput
+            value={nomeAutor}
+            onChangeText={setNomeAutor}
             style={{ backgroundColor: "white", textAlign: "left", padding: 2 }}
             placeholder="nome do autor"
           />
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
-            <TextInput value={editora}
-            onChangeText={setEditora}
+            <TextInput
+              value={editora}
+              onChangeText={setEditora}
               style={{
                 backgroundColor: "white",
                 textAlign: "left",
@@ -70,8 +75,9 @@ export default function App() {
               }}
               placeholder="editora"
             />
-            <TextInput value={preco}
-            onChangeText={setPreco}
+            <TextInput
+              value={preco}
+              onChangeText={setPreco}
               style={{
                 backgroundColor: "white",
                 textAlign: "left",
@@ -81,52 +87,101 @@ export default function App() {
               placeholder="preço"
             />
           </View>
-          <View style={{flexDirection: 'row', justifyContent:"space-between"}}>
-            <TextInput value={tipo}
-            onChangeText={setTipo}
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <TextInput
+              value={tipo}
+              onChangeText={setTipo}
               style={{
                 backgroundColor: "white",
                 textAlign: "left",
                 padding: 2,
-                width: '50%'
+                width: "50%",
               }}
               placeholder="tipo"
             />
-            <View style={{ backgroundColor: "brown", width: '49%' }}>
-              <Text onPress={()=>{
-                const obj = {tituloAutor, nomeAutor, editora, preco, tipo}
-                if(tipo ==  '1'){
-                  setTipoTerror([...tipoTerro, obj])
-                }else{
-                  setTipoAventura([...tipoAventira, obj])
-                }
-
-              }}  style={{textAlign: 'center'}} >Salvar</Text>
+            <View style={{ backgroundColor: "brown", width: "49%" }}>
+              <Text
+                onPress={() => {
+                  const obj = { tituloAutor, nomeAutor, editora, preco, tipo };
+                  if (tipo == "1") {
+                    setTipoTerror([...tipoTerro, obj]);
+                  } else {
+                    setTipoAventura([...tipoAventira, obj]);
+                  }
+                }}
+                style={{ textAlign: "center" }}
+              >
+                Salvar
+              </Text>
             </View>
           </View>
         </View>
-        <View style={{ flex: 2, backgroundColor: 'white' }}>
-          <SectionList 
-          sections={[
-            {title: "livro de terro", data : tipoTerro},
-            {title: 'livro de aventura', data : tipoAventira}
-          ]}
-          renderSectionFooter={({section})=>(
+        <View style={{ flex: 2, backgroundColor: "white" }}>
+          <SectionList
+            sections={[
+              { title: "livro de terro", data: tipoTerro },
+              { title: "livro de aventura", data: tipoAventira },
+            ]}
+            renderSectionHeader={({ section }) => (
+              <View>
+                <Text>{section.title}</Text>
+              </View>
+            )}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                onPress={() => {
+                  setModal(true);
+                }}
+              >
+                <View style={{ backgroundColor: "blue" }}>
+                  <Text>{item.tituloAutor}</Text>
+                  <Text>{item.nomeAutor}</Text>
+                  <Text>{item.editora}</Text>
+                  <Text>{item.preco}</Text>
+                  <Text>{item.tipo}</Text>
+                </View>
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.key}
+          />
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={modal}
+            onRequestClose={() => setModal(false)}
+          >
             <View>
-              <Text>{section.title}</Text>
+              <SectionList
+                sections={[
+                  { title: "livro de terro", data: tipoTerro },
+                  { title: "livro de aventura", data: tipoAventira },
+                ]}
+                renderSectionHeader={({ section }) => (
+                  <View>
+                    <Text>{section.title}</Text>
+                  </View>
+                )}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    onPress={() => {
+                      setModal(false);
+                    }}
+                  >
+                    <View style={{ backgroundColor: "blue" }}>
+                      <Text>{item.tituloAutor}</Text>
+                      <Text>{item.nomeAutor}</Text>
+                      <Text>{item.editora}</Text>
+                      <Text>{item.preco}</Text>
+                      <Text>{item.tipo}</Text>
+                    </View>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.key}
+              />
             </View>
-            
-          )}
-          renderItem={({item})=>(
-            <View>
-              <Text>{item.tituloAutor}</Text>
-              <Text>{item.nomeAutor}</Text>
-              <Text>{item.editora}</Text>
-              <Text>{item.preco}</Text>
-              <Text>{item.tipo}</Text>
-            </View>
-          )} 
-          keyExtractor={item => item.key}/>
+          </Modal>
         </View>
       </View>
     </View>
